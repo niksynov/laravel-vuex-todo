3eed077129df1f3044eb9ea2f434568944816474
Nick Synev <gun15@ukr.net>
2018-03-20 11:24:52 +0200

    Https detection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,9 +13,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $generator)
     {
         Schema::defaultStringLength(191);
+
+        if (env('REDIRECT_HTTPS')) {
+            $generator->forceScheme('https');
+        }
     }
 
     /**
